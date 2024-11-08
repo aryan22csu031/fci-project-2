0bac2c94ed74c71c00a2745acc2cae79c7d86cce
aryan22csu031 <aryan22csu031@ncuindia.edu>
2024-11-08 11:33:19 +0530

    made header and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import { BrowserRouter, Outlet } from "react-router-dom";
 
 function App() {
 
-  return <></>;
+  return <>
+  <BrowserRouter>
+  <div className="w-full h-full">
+    <Header/>
+    <br />
+    <Outlet/>
+    <Footer/>
+  </div>
+  </BrowserRouter>
+  </> 
+  
 }
 
 export default App;
